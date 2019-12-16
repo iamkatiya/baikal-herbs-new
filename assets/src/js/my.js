4350e465c9ef5d129c976968a19b262e29cfbd58
iamkatiya <53205411+iamkatiya@users.noreply.github.com>
2019-12-16 20:23:29 +0800

    katya

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -1,20 +1,47 @@
 $(document).ready(function() {
-    $('.history-carousel').owlCarousel({
-        loop: true,
-        dots: false,
-        nav: true,
-        items: 1,
-        speed: 16000,
+    // $('.history-carousel').owlCarousel({
+    //     loop: true,
+    //     dots: true,
+    //     nav: true,
+    //     items: 1,
+    //     speed: 16000,
+    //     animateOut: 'fadeOut',
+    //     nextArrow: '<a class="arrow-next"></a>',
+    //     prevArrow: '<a class="arrow-prev"></a>',
+    //     responsive: {
+    //         700: {
+    //             items: 1,
+    //         },
+    //         1610: {
+    //             items: 1,
+    //         }
+    //     }
+    // });
+
+    var action = false;
+
+    var owl = $('.history-carousel').owlCarousel({
+        items 	 : 1,
+        center	   : true,
+        nav        : true,
         animateOut: 'fadeOut',
-        nextArrow: '<a class="arrow-next"></a>',
-        prevArrow: '<a class="arrow-prev"></a>',
-        responsive: {
-            700: {
-                items: 1,
-            },
-            1610: {
-                items: 1,
-            }
-        }
+        dots       : true,
+        loop       : true,
+        margin     : 10,
+        dotsContainer: '.test',
+        navText: ['prev','next'],
+    });
+
+    $('.owl-next').on('click',function(){
+        action = 'next'
+    });
+
+    $('.owl-prev').on('click',function(){
+        action = 'prev'
+    });
+
+    $('.bookmarks li').on('click', function(e) {
+        owl.trigger('to.owl.carousel', [$(this).index(), 300]);
     });
 });
+
