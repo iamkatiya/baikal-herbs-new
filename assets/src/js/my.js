e4f654e8d971aa343324de67079af6a412104179
iamkatiya <53205411+iamkatiya@users.noreply.github.com>
2019-12-17 11:35:03 +0800

    save

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -43,19 +43,36 @@ $(document).ready(function() {
     $('.bookmarks li').on('click', function(e) {
         owl.trigger('to.owl.carousel', [$(this).index(), 300]);
     });
+
     $(window).scroll(function() {
         if ($(window).scrollTop() > 10) {
-        $(".header-desktop").addClass('header-scroll');
+            $(".header-desktop").addClass('header-scroll');
             $(".header-nav-menu-item .text-white-uppercase").addClass('black');
             $(".header-nav-menu-item").addClass('black');
             $(".header-nav-menu-absolute").addClass('header-nav-menu-absolute-scroll');
-        }
-        else
-        {
+        } else {
             $(".header-desktop").removeClass('header-scroll');
             $(".header-nav-menu-item .text-white-uppercase").removeClass('black');
             $(".header-nav-menu-absolute").removeClass('header-nav-menu-absolute-scroll');
         }
     });
+
+    $(document).ready(function(){
+        var owl1 = $(".owl-carouselDima").owlCarousel({
+            loop: true,
+            dots: true,
+            nav: true,
+            items: 2,
+            speed: 16000,
+            animateOut: 'fadeOut',
+        });
+
+        $('.teamarks li').on('click', function(e) {
+            owl1.trigger('to.owl.carousel', [$(this).index(), 300]);
+            
+        });
+    });
 });
+
+
 
